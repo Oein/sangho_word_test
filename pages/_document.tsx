1603064f9b8e2af7dc25f840435714dc94612939
Oein <oein0219@gmail.com>
2023-02-19 21:19:44 +0900

    ㅍ1

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="https://unpkg.com/mvp.css@1.12/mvp.css" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
